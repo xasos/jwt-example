3ffce82ff67261740eff6bd08d6afa1f8524112b
Niraj Pant <nirajpant7@gmail.com>
2014-10-13 18:11:51 -0500

    add faker + angular frontend

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var faker = require('faker');
+var cors = require('cors');
 
 var app = express();
+
+app.use(cors());
 
 app.get('/random-user', function(req, res) {
 	var user = faker.helpers.userCard();
 	user.avatar = faker.image.avatar();
 	res.json(user);
-});
-
-app.get('/', function(req, res) {
-	res.json({'message': "Welcome!"});
 });
 
 app.listen(3000, function() {
