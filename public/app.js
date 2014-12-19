58afe962fc5480fc3bc174230ab584a21eda803d
xasos <nirajpant7@gmail.com>
2014-12-19 00:22:12 -0600

    Add logout functionality

--- public/app.js
+++ public/app.js
@@ -1,4 +1,6 @@
-var app = angular.module('app', []);
+var app = angular.module('app', [], function config($httpProvider) {
+	$httpProvider.interceptors.push('AuthInterceptor');
+});
 
 app.constant('API_URL', 'http://localhost:3000');
 
@@ -7,6 +9,7 @@ app.controller('MainCtrl', function MainCtrl(RandomUserFactory, UserFactory) {
 	var vm = this;
 	vm.getRandomUser = getRandomUser;
 	vm.login = login;
+	vm.logout = logout;
 
 
 	function getRandomUser() {
@@ -17,12 +20,18 @@ app.controller('MainCtrl', function MainCtrl(RandomUserFactory, UserFactory) {
 
 	function login(username, password) {
 		UserFactory.login(username, password).then(function success(response) {
-			vm.user = response.data;
+			vm.user = response.data.user;
+			alert(response.data.token);
 		}, handleError);
 	}
 
+	function logout() {
+		UserFactory.logout();
+		vm.user = null;
+	}
+
 	function handleError(response) {
-		alert('Error: ' + repsonse.data);
+		alert('Error: ' + response.data);
 	}
 });
 
@@ -37,16 +46,64 @@ app.factory('RandomUserFactory', function RandomUserFactory($http, API_URL) {
 	}
 });
 
-app.factory('UserFactory', function UserFactory($http, API_URL) {
+app.factory('UserFactory', function UserFactory($http, API_URL, AuthTokenFactory) {
 	'use strict'
 	return {
-		login: login
+		login: login,
+		logout: logout
 	};
 
 	function login(username, password) {
 		return $http.post(API_URL + '/login', {
 			username: username,
 			password: password
+		}).then(function success(response) {
+			AuthTokenFactory.setToken(response.data.token);
+			return response;
 		});
+	}
+
+	function logout() {
+		AuthTokenFactory.setToken();
+	}
+});
+
+app.factory('AuthTokenFactory', function AuthTokenFactory($window) {
+	'use strict';
+	var store = $window.localStorage;
+	var key = 'auth-token';
+
+	return {
+		getToken: getToken,
+		setToken: setToken
+	};
+
+	function getToken() {
+		return store.getItem(key);
+	}
+
+	function setToken(token) {
+		if (token) {
+			store.setItem(key, token);
+		}
+		else {
+			store.removeItem(key);
+		}
+	}
+});
+
+app.factory('AuthInterceptor', function AuthInterceptor(AuthTokenFactory) {
+	'use strict'
+	return {
+		request: addToken
+	};
+
+	function addToken(config) {
+		var token = AuthTokenFactory.getToken();
+		if (token) {
+			config.headers = config.headers || {};
+			config.headers.Authorization = 'Bearer ' + token;
+		}
+		return config;
 	}
 });
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var faker = require('faker');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+
+var jwtsecret = 'omq208fn197gvvbf292';
 
 var user = {
 	username: 'niraj',
@@ -20,7 +23,13 @@ app.get('/random-user', function(req, res) {
 });
 
 app.post('/login', authenticate, function(req, res) {
-	res.send(user);
+	var token = jwt.sign({
+		username: user.username
+	}, jwtsecret);
+	res.send({
+		token: token,
+		user: user
+	});
 });
 
 app.listen(3000, function() {
