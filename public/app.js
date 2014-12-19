7d459159aad61f2ea96d101f01afa471e9ff1fc7
xasos <nirajpant7@gmail.com>
2014-12-18 23:04:09 -0600

    add login functionality

--- public/app.js
+++ public/app.js
@@ -1,27 +1,52 @@
 var app = angular.module('app', []);
 
-	app.constant('API_URL', 'http://localhost:3000');
+app.constant('API_URL', 'http://localhost:3000');
 
-	app.controller('MainCtrl', function MainCtrl(RandomUserFactory) {
-		'use strict';
-		var vm = this;
-		vm.getRandomUser = getRandomUser;
+app.controller('MainCtrl', function MainCtrl(RandomUserFactory, UserFactory) {
+	'use strict';
+	var vm = this;
+	vm.getRandomUser = getRandomUser;
+	vm.login = login;
 
 
-		function getRandomUser() {
-			RandomUserFactory.getUser().then(function success(response) {
-				vm.randomUser = response.data;
-			})
-		}
-	});
+	function getRandomUser() {
+		RandomUserFactory.getUser().then(function success(response) {
+			vm.randomUser = response.data;
+		}, handleError);
+	}
 
-	app.factory('RandomUserFactory', function RandomUserFactory($http, API_URL) {
-		'use strict'
-		return	{
-			getUser: getUser
-		};
+	function login(username, password) {
+		UserFactory.login(username, password).then(function success(response) {
+			vm.user = response.data;
+		}, handleError);
+	}
 
-		function getUser() {
-			return $http.get(API_URL + '/random-user');
-		}
-	});
+	function handleError(response) {
+		alert('Error: ' + repsonse.data);
+	}
+});
+
+app.factory('RandomUserFactory', function RandomUserFactory($http, API_URL) {
+	'use strict'
+	return	{
+		getUser: getUser
+	};
+
+	function getUser() {
+		return $http.get(API_URL + '/random-user');
+	}
+});
+
+app.factory('UserFactory', function UserFactory($http, API_URL) {
+	'use strict'
+	return {
+		login: login
+	};
+
+	function login(username, password) {
+		return $http.post(API_URL + '/login', {
+			username: username,
+			password: password
+		});
+	}
+});
